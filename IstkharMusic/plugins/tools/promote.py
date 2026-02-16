201c5ee56fddc1032e6aa708bae1d8664afa3c64
ignore528 <aliakram7548@gmail.com>
2026-02-16 17:06:28 +0300

    Change import from IstkharMusic to srkMusic

--- IstkharMusic/plugins/tools/promote.py
+++ IstkharMusic/plugins/tools/promote.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters, enums
 from pyrogram.types import ChatPrivileges
 from pyrogram.errors import ChatAdminRequired
 from functools import wraps
-from IstkharMusic import app
+from srkMusic import app
 
 def mention(user_id, name):
     return f"[{name}](tg://user?id={user_id})"
