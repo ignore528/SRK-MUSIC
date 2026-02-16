f01602fd9b01964e562b627ac3e4b876262f418e
ignore528 <aliakram7548@gmail.com>
2026-02-16 19:48:51 +0300

    Update Soundcloud.py

--- IstkharMusic/platforms/Soundcloud.py
+++ IstkharMusic/platforms/Soundcloud.py
@@ -2,7 +2,7 @@ from os import path
 
 from yt_dlp import YoutubeDL
 
-from IstkharMusic.utils.formatters import seconds_to_min
+from srkMusic.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
